9b41da05d86cdf1a6eff7dccba1abdd0d246c306
Benjamin <ayzenbergb@gmail.com>
2017-03-14 12:16:12 -0700

    added Project constructor and sample Project.

---
+++ JS/projects.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Project constructor function
+function Project(name, description, link) {
+  this.name = name;
+  this.description = description;
+  this.link = url;
+}
+
+var salmonCookie = new Project('Salmon Cookie', 'A simple project that taught us the uses of tables and forms.', 'https://bayzenberg.github.io/salmon_cookie/');
