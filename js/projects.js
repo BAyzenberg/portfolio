452dcfc33762de7eb2aeaf56946a8a88549df718
Benjamin <ayzenbergb@gmail.com>
2017-03-20 09:32:22 -0700

    hide everything but the project title

--- js/projects.js
+++ js/projects.js
@@ -5,6 +5,18 @@ Project.prototype.toHTML = function () {
   return template(this);
 };
 
+Project.prototype.preview = function() {
+  $('.project-view *').hide();
+  $('.project-view h1').show();
+
+  // $('.project-view h1').toggle(function() {
+  //   $('this:parent:first-child').fadeIn('fast');
+  // }, function() {
+  //   $('this:parent:first-child').fadeOut('fast');
+  // });
+};
+
 projects.forEach(function(a) {
   $('#project-display').append(a.toHTML());
+  a.preview();
 });
