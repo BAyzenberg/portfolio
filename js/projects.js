f5446490ce9b57928943a74915db085ac6407b77
Benjamin <ayzenbergb@gmail.com>
2017-03-19 19:18:21 -0700

    added handlebars functionality

--- js/projects.js
+++ js/projects.js
@@ -1,14 +1,18 @@
 'use strict';
 
 Project.prototype.toHTML = function () {
-  var $newProject = $('div.template').clone();
-  $newProject.removeClass('template');
+  var template = Handlebars.compile($('#template-output').text());
 
-  $newProject.find('h1').text(this.name);
-  $newProject.find('p').text(this.description);
-  $newProject.find('a').text(this.link);
-  $newProject.find('a').attr('href', this.link);
-  return $newProject;
+  return template(this);
+
+  // var $newProject = $('div.template').clone();
+  // $newProject.removeClass('template');
+  //
+  // $newProject.find('h1').text(this.name);
+  // $newProject.find('p').text(this.description);
+  // $newProject.find('a').text(this.link);
+  // $newProject.find('a').attr('href', this.link);
+  // return $newProject;
 };
 
 projects.forEach(function(a) {
