11b528768cf1a93a97fee442570c0a1bdf3e60b5
Benjamin <ayzenbergb@gmail.com>
2017-03-19 19:33:52 -0700

    removed dead code and unnecissary console logs

--- js/project-list.js
+++ js/project-list.js
@@ -9,8 +9,6 @@ function Project(name, description, url) {
 
 var salmonCookie = new Project('Salmon Cookie', 'A simple project that taught us the uses of tables and forms.', 'https://bayzenberg.github.io/salmon_cookie/');
 
-console.log(salmonCookie);
-
 var textventure = new Project('Textventure', '201 project week choose your own adventure', 'https://carloscadena.github.io/textventure/');
 
 var busmall = new Project('Busmall', 'basic use of event handlers.', 'https://bayzenberg.github.io/bus_mall/');
