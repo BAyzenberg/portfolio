468bacb33a95aaffc259be7054fe7ec0fb2bb94a
Benjamin <ayzenbergb@gmail.com>
2017-03-21 11:20:27 -0700

    added comments to project list

--- js/project-list.js
+++ js/project-list.js
@@ -7,10 +7,12 @@ function Project(name, description, url) {
   this.link = url;
 }
 
+//individual projects
 var salmonCookie = new Project('Salmon Cookie', 'A simple project that taught us the uses of tables and forms.', 'https://bayzenberg.github.io/salmon_cookie/');
 
 var textventure = new Project('Textventure', '201 project week choose your own adventure', 'https://carloscadena.github.io/textventure/');
 
 var busmall = new Project('Busmall', 'basic use of event handlers.', 'https://bayzenberg.github.io/bus_mall/');
 
+//projects array
 var projects = [salmonCookie, textventure, busmall];
