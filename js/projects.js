fe9c1dce1997440ae0feb06b125093504af9d357
Benjamin <ayzenbergb@gmail.com>
2017-03-22 10:01:13 -0700

    functional project print

--- js/projects.js
+++ js/projects.js
@@ -2,6 +2,7 @@
 
 var projects = [];
 
+//Project Constructor
 function Project (rawProject) {
   for(var key in rawProject) {
     this[key] = rawProject[key];
@@ -13,6 +14,13 @@ Project.prototype.toHTML = function() {
   return template(this);
 };
 
+Project.showProjects = function() {
+  projects.forEach(function(a) {
+    $('#project-display').append(a.toHTML());
+    a.preview();
+  });
+};
+
 //accordion preview
 Project.prototype.preview = function() {
   $('.project-view').on('click', function() {
@@ -22,21 +30,18 @@ Project.prototype.preview = function() {
   $('.project-view:first').click();
 };
 
+//Obtain data from the .json
 Project.getData = function() {
   $.getJSON('data/projectList.json')
   .then(function(data) {
     data.forEach(function(singleP) {
       projects.push(new Project(singleP));
-      console.log('a project was pushed');
     });
+    Project.showProjects();
   }).fail(function() {
+    //getJSON failure
     console.error('Data was not obtained');
   });
 };
 
 Project.getData();
-
-projects.forEach(function(a) {
-  $('#project-display').append(a.toHTML());
-  a.preview();
-});
