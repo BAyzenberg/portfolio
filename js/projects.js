aa52721cb9c7f55254a287de713139704f5ba334
Benjamin <ayzenbergb@gmail.com>
2017-03-22 09:40:56 -0700

    get JSON formated but not functional

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var projects = [];
+
+function Project (rawProject) {
+  for(var key in rawProject) {
+    this[key] = rawProject[key];
+  };
+};
+
 Project.prototype.toHTML = function() {
   var template = Handlebars.compile($('#template-output').text());
   return template(this);
@@ -13,6 +21,20 @@ Project.prototype.preview = function() {
   });
   $('.project-view:first').click();
 };
+
+Project.getData = function() {
+  $.getJSON('data/projectList.json')
+  .then(function(data) {
+    data.forEach(function(singleP) {
+      projects.push(new Project(singleP));
+      console.log('a project was pushed');
+    });
+  }).fail(function() {
+    console.error('Data was not obtained');
+  });
+};
+
+Project.getData();
 
 projects.forEach(function(a) {
   $('#project-display').append(a.toHTML());
