82db2711ed0970f126b198d932dba1254b3ada3a
Benjamin <ayzenbergb@gmail.com>
2017-03-22 11:23:09 -0700

    removed clutter

--- js/tab.js
+++ js/tab.js
@@ -14,27 +14,18 @@ tabView.handleNav = function() {
 
 tabView.handleProjects = function() {
   $('.tab[data-content = "projects"]').on('click', function () {
-    console.log('handleProjects is being called');
     projects = [];
-    console.log('initial');
-    console.log(projects);
     if (localStorage.projects) {
-      var data = JSON.parse(localStorage.projects);
-      var temp = [];
-      var testProjects = Project.populateArray(temp, data);
-      if (testProjects === projects) {
-        console.log('temp is equal to projects');
+      var something;
+      if (something) {
         // Project.display(projects, data);
       } else {
-        console.log('data is not equal to projects');
         $('#project-display').empty();
         Project.getData();
       }
     } else {
       Project.getData();
     }
-    console.log('final');
-    console.log(projects);
   });
 };
 
